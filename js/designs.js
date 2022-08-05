b62eb2e948beda36c6424fa5783637740d92e3e5
AnugyaCodingAdventure <anugya.sinha1@outlook.com>
2022-08-05 14:23:02 -0400

    Add comments for methods in designs.js
    Add comments to the methods in designs.js
    fulfil code review comment for deisgns.js

--- js/designs.js
+++ js/designs.js
@@ -7,18 +7,40 @@ const $inputWidth = $('#inputWidth');
 const $colorTableCreator = $('#colorTableCreator');
 const defaultColor = "#000000";
 
+/**
+  * @desc generate a random hex color code
+*/
 function generateRandomColor() {
     var newColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     return newColor;
 }
 
+/**
+  * @desc changes font color of the  title of the page 
+*/
 function ChangePageTitleColor() {
     document.getElementById('PageTitle').style.color = generateRandomColor();
 
 }
 
+/**
+  * @desc calls methods to change font colors of the
+  *       page title every 3.5 seconds.
+*/
 setInterval(ChangePageTitleColor, 3500);
-$colorTableCreator.submit(event => {
+
+/**
+  * @desc binds function onFormSubmitted when submit 
+  *       button of the page form is clicked
+*/
+$colorTableCreator.submit(onFormSubmitted);
+
+/**
+  * @desc removes all existing squares or cells from canvas grid.
+  *       calls method to create a new  canvas with width 
+  *       and height supplied by the user.
+*/
+function onFormSubmitted(event) {
     event.preventDefault();
 
     let width = $inputWidth.val();
@@ -27,8 +49,12 @@ $colorTableCreator.submit(event => {
     $cellCanvas.html('');
 
     makeGrid(height, width);
-});
-
+}
+/**
+  * @desc create a grid of squares
+  * @param int height - number of squares representing the height of the grid
+  * @param  int width - number of squares representing the width of the grid
+*/
 function makeGrid(height, width) {
     for (let i = 0; i < height; i++) {
         let tr = $('<tr></tr>').appendTo($cellCanvas);
@@ -44,18 +70,32 @@ function makeGrid(height, width) {
         .on("mousedown", onCellMouseDown)
         .on("mouseenter", onCellMouseEnter)
         .on("dblclick", onCellDblClick)
-
 }
 
+/**
+  * @desc copies selected color to grid cell/handles click event on any of the cells of the grid
+  * @param object event - jquery wrapped click DOM event
+*/
 function onCellClick(event) {
     let color = getColor();
     $(event.currentTarget).css("background-color", color);
 
 }
+
+/**
+  * @desc remove color from grid cell when cell is double clicked
+  * @param object event - jquery wrapped dblclick DOM event
+*/
 function onCellDblClick(event) {
     $(event.currentTarget).css("background-color", '');
 
 }
+
+/**
+  * @desc copies color to square when numbers 1,2,3,4, or 5 is pressed on
+  *       keyboard keeping cursor on the cell.
+  * @param object event - jquery wrapped keydown DOM event 
+*/
 function onCellKeyDown(event) {
     event.preventDefault();
     $(event.currentTarget).html('');
@@ -67,9 +107,19 @@ function onCellKeyDown(event) {
         $(event.currentTarget).css("background-color", color)
     }
 }
+
+/**
+  * @desc makes the cell focus when mouse is moved over it
+  * @param object event - jquery wrapped DOM mouseover event object
+*/
 function onCellHover(event) {
     event.currentTarget.focus();
 }
+
+/**
+  * @desc get the selected color from the five color pickers, 
+  *       if none are selected default color is returned
+*/
 function getColor() {
     var selectedPicker = $('.color-picker[data-selected="true"]');
     let color = selectedPicker.val();
@@ -78,27 +128,52 @@ function getColor() {
     return color;
 }
 
+/**
+  * @desc Marks a color picker selected when color picker is 
+  *       focused or color selection popup is closed. Applies
+  *       selected to color to Selected Color tag for indication.
+  * @param object event -  DOM focus or input DOM event object
+*/
 function onSelection(event) {
     let pickers = document.querySelectorAll(".color-picker");
     pickers.forEach(picker => picker.dataset.selected = false);
     event.currentTarget.dataset.selected = "true";
     $("#h1SelectedColorIndicator").css("color", event.currentTarget.value);
 }
+
+/**
+  * @desc marks dragging start when mouse is clicked over a cell.
+  *       copies color of the  cell to a variable  so that it can
+  *       be copied to other cells.
+  * @param object event -  DOM mouse down  DOM event object
+*/
 function onCellMouseDown(event) {
     dragging = true;
     selectedBackgroundColor = event.currentTarget.style.backgroundColor;
-    console.log(selectedBackgroundColor);
 }
 
+/**
+  * @desc Copies the color of cells where mouse drag 
+  *       started to the cell where mouse drag has entered.
+  * @param object event -  DOM mouseenter  DOM event object
+*/
 function onCellMouseEnter(event) {
     if (dragging == false) return;
     event.currentTarget.style.backgroundColor = selectedBackgroundColor;
 }
+
+/**
+  * @desc marks mouse drag as stopped when mouse click is released.
+*/
 function onMouseUp() {
     dragging = false;
 }
 
-
+/**
+  * @desc binds onSelection method to color pickers to handle focus 
+  *       and input events on DOM ready event.
+  *       binds onMouseup event to the web page when
+*/
 $(document).ready(function () {
     let pickers = document.querySelectorAll(".color-picker");
     pickers.forEach(picker => {
